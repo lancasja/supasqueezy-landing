f8290085b1d2d724a8de455be9bdf121b68ac85e
Jonathan Lancaster <lancasja@gmail.com>
2025-09-18 18:03:05 -0700

    chore: enable unoptimized images in next.config.mjs

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -141,7 +141,7 @@ const HeroSection: React.FC = () => {
             <FallInPlace delay={1}>
               <Box overflow="hidden" height="100%">
                 <Image
-                  src="public/static/screenshots/list.png"
+                  src="/static/screenshots/list.png"
                   width={1200}
                   height={762}
                   alt="Screenshot of a ListPage in Saas UI Pro"
