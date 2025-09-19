1c88d11d688abfc2fa6e8f240926e5d0d8371017
Jonathan Lancaster <lancasja@gmail.com>
2025-09-18 19:00:15 -0700

    refactor: remove ChakraProvider from Provider component and apply theme directly to SaasProvider

--- app/provider.tsx
+++ app/provider.tsx
@@ -1,6 +1,5 @@
 'use client'
 
-import { ChakraProvider } from '@chakra-ui/react'
 import { AuthProvider } from '@saas-ui/auth'
 import { SaasProvider } from '@saas-ui/react'
 
@@ -8,10 +7,8 @@ import { theme } from '#theme'
 
 export function Provider(props: { children: React.ReactNode }) {
   return (
-    <ChakraProvider theme={theme}>
-      <SaasProvider>
-        <AuthProvider>{props.children}</AuthProvider>
-      </SaasProvider>
-    </ChakraProvider>
+    <SaasProvider theme={theme}>
+      <AuthProvider>{props.children}</AuthProvider>
+    </SaasProvider>
   )
 }
